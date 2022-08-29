4a159f77c31f5bb5776c63a94c11b00b36230ea8
miguel.developer.angel <miguel.developer.angel@gmail.com>
2022-08-30 00:44:12 +0200

    back to form with getform

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -5,8 +5,10 @@ import * as styles from './formulario.module.css'
 const Formulario = () => (
   <div>
     <p>Quieres que te llamemos ?</p>
-    <form name="contact" netlify data-netlify-honeypot="bot-field" hidden>
-      <input type="hidden" name="form-name" value="contact" />
+    <form
+      action="https://getform.io/f/707d3d5f-e394-4a94-990c-a6d5c8d20133"
+      method="POST"
+    >
       <p>
         <input type="text" name="nombre" placeholder="Nombre" />
       </p>
